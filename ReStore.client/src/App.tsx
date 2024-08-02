e60f897298a41f8c7785e23e408bc12ad72b30e5
Gabriel Cordeiro <potatomexicano006@yahoo.com>
2024-08-02 08:30:40 -0300

    [feat] adicionado state de produtos.
    * Adicionado lista de produtos com botão para adicionar novos.

--- ReStore.client/src/App.tsx
+++ ReStore.client/src/App.tsx
@@ -1,9 +1,15 @@
-const products = [
-  {name: 'product1', price: 100.00},
-  {name: 'product2', price: 200.00}
-]
+import { useState } from "react"
 
 function App() {
+  const [products, setProducts] = useState([
+    {name: 'product1', price: 100.00},
+    {name: 'product2', price: 200.00}
+  ]);
+
+  function addProducts() {
+    setProducts(prevState => [...prevState, {name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100}]);
+  }
+
   return (
     <div className="app">
       <h1>Re-Store</h1>
@@ -12,6 +18,7 @@ function App() {
           <li key={index}>{item.name} - {item.price}</li>
         ))}
       </ul>
+      <button onClick={addProducts}>Add product</button>
     </div>
   )
 }
