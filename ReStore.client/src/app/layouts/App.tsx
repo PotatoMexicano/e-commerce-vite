9cfdbfde413d8587d8a0d163a294b5c602e5c920
Gabriel Cordeiro <potatomexicano006@yahoo.com>
2024-08-12 08:29:02 -0300

    [feat] Adicionado react-router-dom
    * Adicionado páginas de About, Contact e ProductDetails.
    * Adicionado reactRouter.

--- ReStore.client/src/app/layouts/App.tsx
+++ ReStore.client/src/app/layouts/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import Catalog from "../../features/catalog/Catalog";
 import Header from "./Headers";
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { Outlet } from "react-router-dom";
 
 function App() {
 
@@ -26,7 +27,7 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog />
+        <Outlet />
       </Container>
     </ThemeProvider>
   )
