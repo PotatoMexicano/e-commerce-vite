0c388f490055e64b27fd9f81d30d61c38f697995
Gabriel Cordeiro <potatomexicano006@yahoo.com>
2024-08-20 08:24:08 -0300

    [feat]
    * Adicionado mapeamento para erros 404 e 500.
    * Adicionado delay de 500ms nos requests.
    * Adicionado página personalizada de erros 404 e 500.
    * Adicionado componente de carregamento.

--- ReStore.client/src/features/catalog/Catalog.tsx
+++ ReStore.client/src/features/catalog/Catalog.tsx
@@ -3,14 +3,21 @@ import { Product } from "../../app/modules/products"
 import ProductList from "./ProductList";
 import { useEffect, useState } from "react";
 import agent from "../../app/api/agent";
+import LoadingComponent from "../../app/layouts/LoadingComponent";
 
 export default function Catalog() {
 
     const [products, setProducts] = useState<Product[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        agent.Catalog.list().then(products => setProducts(products))
+        agent.Catalog.list()
+        .then(products => setProducts(products))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false));
     }, []);
+
+    if (loading) return <LoadingComponent message="Loading catalog" />
 
     return (
         <>
