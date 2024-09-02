092be9beb933e9cfbe76bd09c42090116986402b
Gabriel Cordeiro <potatomexicano006@yahoo.com>
2024-09-02 08:30:41 -0300

    [feat] Adicionado formatador de moedas padrão.

--- ReStore.client/src/features/catalog/ProductCard.tsx
+++ ReStore.client/src/features/catalog/ProductCard.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import agent from "../../app/api/agent";
 import { LoadingButton } from "@mui/lab";
 import { useStoreContext } from "../../app/context/StoreContext";
+import { currencyFormat } from "../../app/util/util";
 
 interface Props {
     product: Product;
@@ -36,7 +37,7 @@ export default function ProductCard({ product }: Props) {
             <CardMedia sx={{ height: 140, backgroundSize: 'contain', bgcolor: 'primary.light' }} image={product.pictureUrl} title={product.name} />
             <CardContent>
                 <Typography gutterBottom color='secondary' variant="h5" component="div">
-                    ${(product.price / 100).toFixed(2)}
+                    {currencyFormat(product.price)}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
                     {product.brand} / {product.type}
